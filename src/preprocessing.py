265c2a2faed09070e82746dfeacd679c05cc8911
Martin Le Bras <martin.lebras0@gmail.com>
2024-10-28 17:14:54 +0100

    ml/Adding preprocessing script

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -4,7 +4,9 @@ from nltk.corpus import stopwords
 import re
 import os
 import collections
-
+import pickle
+import numpy as np
+import gc
 
 def load_data(train_path, dev_path, test_path, corpus_path):
     # Load the CSV files into pandas DataFrames
@@ -49,6 +51,15 @@ def split_data_by_language(df):
     language_dfs = {lang: df[df['lang'] == lang] for lang in df['lang'].unique()}
     return language_dfs
 
+def create_language_corpus(corpus: list) -> dict:
+    language_corpus = {"en":[],"fr":[],"es":[],"de":[],"it":[],"ar":[],"ko":[]}
+    for document in corpus:
+        language_corpus[document["lang"]].append(document)
+    for language in language_corpus.keys():
+        with open(f"data/corpus_{language}.pkl","wb",encoding='utf-8') as file:
+            pickle.dump(language_corpus[language],file)
+    del language_corpus
+
 def save_cleaned_data(cleaned_data, output_dir='cleaned_data'):
     """ Saves cleaned DataFrames as JSON files in the specified directory. """
     if not os.path.exists(output_dir):
@@ -59,9 +70,43 @@ def save_cleaned_data(cleaned_data, output_dir='cleaned_data'):
         output_path = os.path.join(output_dir, f'cleaned_data_{lang}.json')
         df.to_json(output_path, orient='records', lines=True)  # Save as JSON lines
 
+def get_document_vocabulary(words: str) -> dict:
+    return dict(collections.Counter(words))
+
+def get_corpus_frequencies(corpus: list, language: str) -> dict:
+    corpus_dict = {}
+    for document in corpus:
+        corpus_dict[document["docid"]] = get_document_vocabulary(preprocess_data(document["text"].split(" "),language))
+    return corpus_dict
+
+def get_corpus_vocabulary(corpus_frequencies: dict) -> list:
+    vocabulary = list()
+    for doc_id in corpus_frequencies.keys():
+        document_vocabulary = list(corpus_frequencies[doc_id].keys())
+        vocabulary.extend(document_vocabulary)
+    return list(set(vocabulary))
+
+def inverse_document_frequency(corpus_frequencies: dict, corpus_vocabulary: list) -> dict:
+
+    document_count_per_word = collections.defaultdict(int)
+    
+    for doc_id, word_freq in corpus_frequencies.items():
+        for word in word_freq.keys():
+            document_count_per_word[word] += 1
+    
+    num_documents = len(corpus_frequencies)
+    
+    idf = {}
+    for word in corpus_vocabulary:
+        n_word = document_count_per_word[word]
+        idf[word] = float(np.log(num_documents / n_word))
+
+    return idf
+
 # Testing block
 if __name__ == '__main__':
-    # Step 1: Load all the data (train, dev, test, and corpus)
+
+    """# Step 1: Load all the data (train, dev, test, and corpus)
     train_df, dev_df, test_df, corpus_df = load_data(
         'data/train.csv',
         'data/dev.csv',
@@ -85,4 +130,34 @@ if __name__ == '__main__':
     save_cleaned_data(cleaned_train_data, output_dir='cleaned_train_data')
     save_cleaned_data(cleaned_dev_data, output_dir='cleaned_dev_data')
     save_cleaned_data(cleaned_test_data, output_dir='cleaned_test_data')
-    save_cleaned_data(cleaned_corpus_data, output_dir='cleaned_corpus_data')
+    save_cleaned_data(cleaned_corpus_data, output_dir='cleaned_corpus_data')"""
+
+    JSON_PATH = "data/corpus.json"
+    with open(JSON_PATH,"r",encoding='utf-8') as file:
+        data = json.load(file)
+
+    print("Creating corpus for each language")
+    create_language_corpus(data)
+    print("Corpus created for each language")
+    gc.collect()
+    languages = ["en","fr","it","es","de","ar","ko"]
+    for language in languages:
+        print(f"Loading corpus in {language}")
+        with open(f"data/corpus_{language}.pkl","rb",encoding='utf-8') as corpus_file:
+            corpus = pickle.load(corpus_file)
+        print(f"Corpus loaded in {language}")
+
+        corpus_frequencies = get_corpus_frequencies(corpus, language)
+        with open(f"data/corpus_freq_{language}.pkl","wb",encoding='utf-8') as freq_file:
+            pickle.dump(corpus_frequencies, freq_file)
+        print(f"Saved the {language} corpus frequency as data/corpus_freq_{language}.pkl")
+
+        corpus_vocabulary = get_corpus_vocabulary(corpus_frequencies)
+        with open(f"data/corpus_vocab_{language}.pkl","wb",encoding='utf-8') as vocab_file:
+            pickle.dump(corpus_vocabulary, vocab_file)
+        print(f"Saved the {language} corpus vocabulary as data/corpus_vocab_{language}.pkl")
+
+        idf = inverse_document_frequency(corpus_frequencies, corpus_vocabulary)
+        with open(f"data/corpus_idf_{language}.pkl","wb",encoding='utf-8') as idf_file:
+            pickle.dump(idf, idf_file)
+        print(f"Saved the {language} corpus idf as data/corpus_idf_{language}.pkl")
--- src/utils.py
+++ src/utils.py
@@ -2,41 +2,25 @@ import collections
 import numpy as np
 from preprocessing import *
 
-def get_document_vocabulary(words: str) -> dict:
-    return dict(collections.Counter(words))
-
-def get_corpus_frequencies(corpus: list, language: str) -> dict:
-    corpus_dict = {}
-    for document in corpus:
-        corpus_dict[document["docid"]] = get_document_vocabulary(preprocess_data(document["text"].split(" "),language))
-    return corpus_dict
-
-def get_corpus_vocabulary(corpus_frequencies: dict) -> list:
-    vocabulary = list()
-    for doc_id in corpus_frequencies.keys():
-        document_vocabulary = list(corpus_frequencies[doc_id].keys())
-        vocabulary.extend(document_vocabulary)
-    return list(set(vocabulary))
-
 def term_frequency(word: str, document_frequency: dict) -> float:
     return document_frequency[word] / max(document_frequency.values())
 
-def inverse_document_frequency(corpus_frequencies: dict, corpus_vocabulary: list) -> dict:
-
-    document_count_per_word = collections.defaultdict(int)
-    
-    for doc_id, word_freq in corpus_frequencies.items():
-        for word in word_freq.keys():
-            document_count_per_word[word] += 1
-    
-    num_documents = len(corpus_frequencies)
-    
-    idf = {}
-    for word in corpus_vocabulary:
-        n_word = document_count_per_word[word]
-        idf[word] = float(np.log(num_documents / n_word))
-    
-    return idf
-
 def tf_idf(word: str, document_vocabulary: dict, idf: dict) -> float:
     return term_frequency(word, document_vocabulary)*idf[word]
+
+def avg_doc_length(corpus_frequencies):
+    total_length = 0
+    count = 0
+    for doc_id, doc_freq in corpus_frequencies.items():
+        total_length += sum(list(doc_freq.values()))
+        count += 1
+    return total_length/count
+
+def bm25(query,idf,doc_freq,avg_length,k1,b):
+    doc_length = sum(list(doc_freq.values()))
+    bm_score = 0
+    for word in query.keys():
+        if word in idf:
+            if word in doc_freq:
+                bm_score += idf[word]*(doc_freq[word]*(k1 + 1))/(doc_freq[word] + k1*(1-b + b*doc_length/avg_length))
+    return bm_score
